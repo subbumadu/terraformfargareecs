1ee30789c092d4f7deba2fd2b70438208453c03e
subbumadu <subbumadu8@gmail.com>
2023-04-11 06:25:16 +0000

    ok

--- vpc.tf
+++ vpc.tf
@@ -45,26 +45,3 @@ resource "aws_eip" "test-eip" {
   depends_on = [aws_internet_gateway.test-igw]
 }
 
-resource "aws_nat_gateway" "test-natgw" {
-  count         = var.az_count
-  subnet_id     = element(aws_subnet.public.*.id, count.index)
-  allocation_id = element(aws_eip.test-eip.*.id, count.index)
-}
-
-# Create a new route table for the private subnets, make it route non-local traffic through the NAT gateway to the internet
-resource "aws_route_table" "private" {
-  count  = var.az_count
-  vpc_id = aws_vpc.test-vpc.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = element(aws_nat_gateway.test-natgw.*.id, count.index)
-  }
-}
-
-# Explicitly associate the newly created route tables to the private subnets (so they don't default to the main route table)
-resource "aws_route_table_association" "private" {
-  count          = var.az_count
-  subnet_id      = element(aws_subnet.private.*.id, count.index)
-  route_table_id = element(aws_route_table.private.*.id, count.index)
-}
